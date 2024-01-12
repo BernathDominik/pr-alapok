eae32a397b0ebab52131a630cedda321ac104c6d
BernathDominik <113088388+BernathDominik@users.noreply.github.com>
2024-01-12 11:52:06 +0100

    Add files via upload

---
+++ Feladat/feladat_030.py
@@ -0,0 +1,18 @@
+# feladat_030
+
+honapok = ['január', 'február','március', 'április', 'május', 'június']
+
+for honap in honapok:
+    honap = honap.upper()
+    print(honap)    
+print(honapok)
+
+for index in range(len(honapok)):
+    honapok[index] = honapok[index].upper()
+print(honapok)
+
+for index in range(0,len(honapok),2):
+    honapok[index] = honapok[index].lower()
+print(honapok)
+
+print(honapok.sort)
